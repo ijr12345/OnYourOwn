f758ec1ff0be3143e06692c8e25518327253fd1c
Peter-Hafner <hafnerp2@wwu.edu>
2023-11-17 22:38:23 -0800

    Added results page and connected it to survey

--- public/scripts/surveyPopup.js
+++ public/scripts/surveyPopup.js
@@ -5,6 +5,7 @@ document.getElementById("surveyButton").addEventListener("click", function() {
 document.getElementById("submitSurvey").addEventListener("click", function() {
   alert("Survey submitted.");
   document.getElementById("overlay").style.display = "none";
+  window.location.href = "pages/results.html";
 });
 
 document.getElementById("closeSurvey").addEventListener("click", function() {
